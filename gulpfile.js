71c75956bafefc336896f18558d61205aaacd4f3
Ilya.Meer <ilya.meerov@gmail.com>
2017-10-19 14:23:07 -0700

    Initiated sass, separated css into sass partials

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,24 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify'),
     browserSync = require('browser-sync').create(),
     rename = require('gulp-rename'),
-    eslint = require('gulp-eslint');
+    eslint = require('gulp-eslint'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano')
+    prettyError = require('gulp-prettyerror');
+    
+    gulp.task('sass', function() {
+      gulp.src('./sass/style.scss')
+         .pipe(prettyError()) //error handling
+         .pipe(sass())
+         .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+         }))
+         .pipe(gulp.dest('./build/css'))
+         .pipe(cssnano())
+         .pipe(rename('style.min.css'))
+         .pipe(gulp.dest('./build/css'));
+   });
 
 gulp.task('script', ['eslint'], function(){
 gulp.src('./js/*.js') // What files do we want gulp to consume?
@@ -21,6 +38,7 @@ gulp.task('eslint', function() {
  
 gulp.task('watch', function() {
   gulp.watch('js/*.js', ['script']);
+  gulp.watch('sass/*.scss', ['sass']);
 });
 
 
@@ -39,3 +57,37 @@ gulp.task('browser-sync', function(){
 
 // Modify our default task method by passing an array of task names
 gulp.task('default', ['watch', 'browser-sync']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// gulp.task('sass', function() {
+// gulp.src('./sass/style.scss')
+//   .pipe(sass())
+//   .pipe(autoprefixer({
+//      browsers: ['last 2 versions']
+//   }))
+//   .pipe(gulp.dest('./build/css'))
+//   .pipe(cssnano())
+//   .pipe(rename('style.min.css'))
+//   .pipe(gulp.dest('./build/css'));
+// });
+
+
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,37 +1,45 @@
 $(function(){
+  $('#menu').on('change', function(event) {
+  event.preventDefault();
   
+  if ($(window).width() < 600){
+      $('header').removeClass('main');
+      $('header').addClass('add-photos');
+    } else if (($(window).width() < 600)) {
+      $('header').removeClass('main');
+      $('header').addClass('add-photos-tablet');
+    } else {
+      $('header').removeClass('main');
+      $('header').addClass('add-photos-desktop');
+    }
 
-$('button').on('click', function(data) {
 
-  // $('.main').append('<img class="loader" src="assets/images/ajax-loader.gif">');  
 
-  $.ajax({
-    method: 'GET',
-    url: 'https://api.nytimes.com/svc/topstories/v2/books.json?api-key=1d3d67d37ae24c4a835479a2882cd51d',
-        
-  })
 
+var selected = $('#menu option:selected' ).val();
+var url = "https://api.nytimes.com/svc/topstories/v2/"+ selected + ".json";
+  url += '?' + $.param({
+    'api-key': "1d3d67d37ae24c4a835479a2882cd51d"
+});
+$.ajax({
+  url: url,
+  method: 'GET'
+})
   .done(function(data) {
-   $.each(data, function(index, value){
-     console.log(index);
-     $('ul').append('<li>' + index + ":" + value + '</li>');
-    
-    })   
-  })
-       .fail(function() {
-        
-  })
-       .always(function(){
-        //  ('.loader').remove();
-       })
+    $('ul').empty();
+    for(var i = 0; i <= 11; i++) {
+    var title = data.results[i].abstract
+    var image = data.results[i].multimedia[2].url
+    console.log(title);
+    $('ul').append('<li style="background: url(' + image + ')";><p>' + title + '</p>' + '<img src="' + image + '">' + '</li>');
+  }
+
+})
+  .fail(function() {
+    console.log(url);
 });
 
 });
 
 
-
-
-
-
-
-
+});
