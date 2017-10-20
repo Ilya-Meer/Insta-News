800423243e96a8c2e49f9e117d354e0d18121dbe
Ilya.Meer <ilya.meerov@gmail.com>
2017-10-19 17:39:02 -0700

    arranged layout of news items

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(function(){
   if ($(window).width() < 600){
       $('header').removeClass('main');
       $('header').addClass('add-photos');
-    } else if (($(window).width() < 600)) {
+    } else if (($(window).width() < 1000)) {
       $('header').removeClass('main');
       $('header').addClass('add-photos-tablet');
     } else {
@@ -13,33 +13,30 @@ $(function(){
       $('header').addClass('add-photos-desktop');
     }
 
+  var selected = $('#menu option:selected' ).val();
+  var url = "https://api.nytimes.com/svc/topstories/v2/"+ selected + ".json";
+    url += '?' + $.param({
+      'api-key': "1d3d67d37ae24c4a835479a2882cd51d"
+  });
 
-
-
-var selected = $('#menu option:selected' ).val();
-var url = "https://api.nytimes.com/svc/topstories/v2/"+ selected + ".json";
-  url += '?' + $.param({
-    'api-key': "1d3d67d37ae24c4a835479a2882cd51d"
-});
-$.ajax({
+  $.ajax({
   url: url,
   method: 'GET'
-})
+  })
   .done(function(data) {
-    $('ul').empty();
+    $('.headlines').empty();
     for(var i = 0; i <= 11; i++) {
     var title = data.results[i].abstract
-    var image = data.results[i].multimedia[2].url
+    var image = data.results[i].multimedia[4].url
+    var url = data.results[i].url
     console.log(title);
-    $('ul').append('<li style="background: url(' + image + ')";><p>' + title + '</p>' + '<img src="' + image + '">' + '</li>');
-  }
-
-})
+    $('.headlines').append('<a style="background-image: url(' + image + ')" href="' + url + '"' + '>'  + '<div class="list-image">' + title + '</div></a>');
+    }
+  })
   .fail(function() {
     console.log(url);
-});
+  });
 
 });
-
 
 });
