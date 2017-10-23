3fd1b40d370c5d3ddc32e31dd88b20cdfcc07af3
Ilya.Meer <ilya.meerov@gmail.com>
2017-10-22 17:14:47 -0700

    Positioned loader

--- js/script.js
+++ js/script.js
@@ -1,42 +1,45 @@
-$(function(){
-  $('#menu').on('change', function(event) {
-  event.preventDefault();
-  
-  if ($(window).width() < 600){
-      $('header').removeClass('main');
-      $('header').addClass('add-photos');
-    } else if (($(window).width() < 1000)) {
-      $('header').removeClass('main');
-      $('header').addClass('add-photos-tablet');
+$(function () {
+
+  $('#menu').on('change', function (event) {
+    event.preventDefault();
+    $('header').addClass('add-photos');
+    $('.headlines').empty();
+
+    if ($('select') === '') {
+      $('.loader').hide();
     } else {
-      $('header').removeClass('main');
-      $('header').addClass('add-photos-desktop');
+      $('.loader').show();
     }
 
-  var selected = $('#menu option:selected' ).val();
-  var url = "https://api.nytimes.com/svc/topstories/v2/"+ selected + ".json";
+   
+    var selected = $('#menu option:selected').val();
+    var url = "https://api.nytimes.com/svc/topstories/v2/" + selected + ".json";
     url += '?' + $.param({
       'api-key': "1d3d67d37ae24c4a835479a2882cd51d"
+    });
+
+    $.ajax({
+      url: url,
+      method: 'GET'
+    })
+      .done(function (data) {
+        data.results.filter(function (item) {
+          if (item.multimedia.length !== 0) {
+            return item
+          }
+        }).splice(0, 12).forEach(function (item) {
+          var title = item.abstract
+          var image = item.multimedia[item.multimedia.length - 1].url
+          var url = item.url
+          $('.headlines').append('<a style="background-image: url(' + image + ')" href="' + url + '"' + '>' + '<div class="list-image">' + title + '</div></a>');
+          $('.loader').hide();
+          
+        })
+      }).fail(function () {
+        $('.headlines').append('<h3>Sorry! There was a problem, please try again!</h3>');
+
+      }).always(function () {
+        
+      });
   });
-
-  $.ajax({
-  url: url,
-  method: 'GET'
-  })
-  .done(function(data) {
-    $('.headlines').empty();
-    for(var i = 0; i <= 11; i++) {
-    var title = data.results[i].abstract
-    var image = data.results[i].multimedia[4].url
-    var url = data.results[i].url
-    console.log(title);
-    $('.headlines').append('<a style="background-image: url(' + image + ')" href="' + url + '"' + '>'  + '<div class="list-image">' + title + '</div></a>');
-    }
-  })
-  .fail(function() {
-    console.log(url);
-  });
-
-});
-
 });
