02e7a27b541ae67d5a52e20c624d0d9daa658fa0
Ilya.Meer <ilya.meerov@gmail.com>
2017-10-23 14:57:45 -0700

    Added Selectric to style select menu

--- js/script.js
+++ js/script.js
@@ -1,21 +1,15 @@
 $(function () {
-
   $('#menu').on('change', function (event) {
-    event.preventDefault();
+    event.preventDefault;
     $('header').addClass('add-photos');
     $('.headlines').empty();
+    $('.headlines').append('<img class="loader" src="assets/images/ajax-loader1.gif">');
 
-    if ($('select') === '') {
-      $('.loader').hide();
-    } else {
-      $('.loader').show();
-    }
 
-   
     var selected = $('#menu option:selected').val();
-    var url = "https://api.nytimes.com/svc/topstories/v2/" + selected + ".json";
+    var url = 'https://api.nytimes.com/svc/topstories/v2/' + selected + '.json';
     url += '?' + $.param({
-      'api-key': "1d3d67d37ae24c4a835479a2882cd51d"
+      'api-key': '1d3d67d37ae24c4a835479a2882cd51d'
     });
 
     $.ajax({
@@ -25,21 +19,35 @@ $(function () {
       .done(function (data) {
         data.results.filter(function (item) {
           if (item.multimedia.length !== 0) {
-            return item
+            return item;
           }
         }).splice(0, 12).forEach(function (item) {
-          var title = item.abstract
-          var image = item.multimedia[item.multimedia.length - 1].url
-          var url = item.url
-          $('.headlines').append('<a style="background-image: url(' + image + ')" href="' + url + '"' + '>' + '<div class="list-image">' + title + '</div></a>');
-          $('.loader').hide();
-          
-        })
-      }).fail(function () {
-        $('.headlines').append('<h3>Sorry! There was a problem, please try again!</h3>');
+          var title = item.abstract;
+          var image = item.multimedia[item.multimedia.length - 1].url;
+          var url = item.url;
+          $('.headlines').append('<a class="anchor" style="background-image: url(' + image + ')" href="' + url + '"' + '>' + '<div class="list-image">' + title + '</div></a>');
+
+          $('.anchor').on('mouseover', function () {
+            $(this).children().css('visibility', 'visible');
+          })
+          $('.anchor').on('mouseleave', function () {
+            $(this).children().css('visibility', 'hidden');
+          })
+
+        });
+
+      })
+      .fail(function () {
+        console.log('Whoops!');
+        $('footer').prepend('<h3>Sorry! There was a problem, please try again!</h3>');
 
       }).always(function () {
-        
+        $('.loader').remove();
+        $('h3').remove();
       });
   });
+
+  $('select').selectric();
 });
+
+
