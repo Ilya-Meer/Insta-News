591a2c891e5cd3b6b599412ae815ad066ff077ce
Ilya <ilya.meerov@gmail.com>
2019-04-09 22:38:49 -0700

    Refactor main script file

--- js/script.js
+++ js/script.js
@@ -1,71 +1,87 @@
-import '../sass/style.scss';
+import NYT_API_KEY from '../config';
+
 import $ from 'jquery';
+import '../sass/style.scss';
 import '../lib/jquery.selectric';
 
-$(document).ready(function(){
+const STORIES_QTY = 12;
+
+const startLoading = () => {
+  $('header').addClass('add-photos');
+  $('.headlines').empty();
+  $('footer').hide();
+  $('.loader-div').append('<img class="loader" src="assets/images/ajax-loader1.gif">');
+};
+
+const stopLoading = () => {
+  $('.loader-div').empty();
+};
+
+const displayError = () => {
+  stopLoading();
+  $('.loader-div').append('<h3>Sorry! There was a problem, please try again!</h3>');
+};
+
+$(document).ready(() => {
   window.addEventListener('touchstart', function onFirstTouch() {
-    document.body.classList.add('touch');
+    $('body').addClass('touch');
   });
 
-  $(function () {
-    $('#menu').on('change', function (event) {
-      event.preventDefault;
-      $('header').addClass('add-photos');
-      $('.headlines').empty();
-      $('footer').hide();
-      $('.loader-div').append('<img class="loader" src="assets/images/ajax-loader1.gif">');
-  
-    // Generating API URL
-  
-      let selected = $('#menu option:selected').val();
-      let url = 'https://api.nytimes.com/svc/topstories/v2/' + selected + '.json';
-      url += '?' + $.param({ 'api-key': '1d3d67d37ae24c4a835479a2882cd51d'
-    });
-  
-    // Fetching Content
-  
-      $.ajax({
-        url: url,
-        method: 'GET'
-      })
-        .done(function (data) {
-          data.results.filter(function (item) {
+  $('#menu').on('change', event => {
+    event.preventDefault();
+    startLoading();
+
+  // Generating API URL
+
+    const selected = $('#menu option:selected').val();
+    const queryParams = `?${$.param({ 'api-key': NYT_API_KEY })}`;
+    const url = `https://api.nytimes.com/svc/topstories/v2/${selected}.json${queryParams}`;
+
+  // Fetching Content
+
+    $.ajax({
+      url: url,
+      method: 'GET'
+    })
+      .done(data => {
+        data.results
+          .filter(item => {
             if (item.multimedia.length !== 0) {
               return item;
             }
-          }).splice(0, 12).forEach(function (item) {
-            let title = item.abstract;
-            let image = item.multimedia[item.multimedia.length - 1].url;
-            let url = item.url;
-            $('.headlines').append(
-                '<a class="anchor"' +  
-                'target="_blank"' +
-                'rel="noopener"' +
-                'href="' + url + '"' + '>' + 
-                '<div class="thumbnail-wrapper">' +
-                '<img src="' + image + '"/>' +
-                '</div>' +
-                '<div class="list-image">' + title + '</div></a>');
-            $('.anchor').on('mouseover', function () {
-              $(this).children('.list-image').css('visibility', 'visible');
-            })
-            $('.anchor').on('mouseleave', function () {
-              $(this).children('.list-image').css('visibility', 'hidden');
-            })
-          });
-          $('footer').show();
-        })
-        .fail(function () {
-          $('.loader-div').empty();
-          $('.loader-div').append('<h3>Sorry! There was a problem, please try again!</h3>');
-        }).always(function () {      
-          $('.loader-div').empty();   
-        });
+          })
+          .splice(0, STORIES_QTY)
+            .forEach(item => {
+              const title = item.abstract;
+              const image = item.multimedia[item.multimedia.length - 1].url;
+              const url = item.url;
+
+              $('.headlines').append(
+                `<a class="anchor" target="_blank" rel="noopener" href="${url}">
+                  <div class="thumbnail-wrapper">
+                    <img src="${image}"/>
+                  </div>
+                  <div class="list-image">${title}</div>
+                </a>
+              `);
+
+              $('.anchor').on('mouseover', function () {
+                $(this).children('.list-image').css('visibility', 'visible');
+              })
+
+              $('.anchor').on('mouseleave', function () {
+                $(this).children('.list-image').css('visibility', 'hidden');
+              })
+            });
+        $('footer').show();
+      })
+      .fail(function () {
+        displayError();
+      }).always(function () {      
+        stopLoading();
       });
-  
-    // Initialize Selectric
-    $('select').selectric();
-  });
-})  
+    });
 
-
+  // Initialize Selectric
+  $('select').selectric();
+});
